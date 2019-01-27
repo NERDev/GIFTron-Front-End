081b99a726b26a73f5b8e5ce1344e318db9826b2
christian <christian.kramer@mail.com>
2019-01-27 15:54:05 -0600

    working user avatar example

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
+Vue.component('user-avatar', {
+    template: `<img v-bind:src="'https://cdn.discordapp.com/avatars/' + user.id + '/' + user.avatar + '.png'" />`,
+    props: ['user'],
+});
+
 var app = new Vue({
     el: 'main',
-    data: {
-        message: 'hello world!'
+    data() {
+        return {
+            user: {},
+            loading: true
+        };
     }
 });
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        // Action to be performed when the document is read;
+        app.user = JSON.parse(this.response);
+        app.loading = false;
+    }
+};
+xhttp.open("GET", "api/v1/user", true);
+xhttp.send();
